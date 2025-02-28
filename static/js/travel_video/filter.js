618f33b83eef880ecfdc3ac051c097d2c63d2312
jliang353 <jliang353@gatech.edu>
2025-02-27 21:28:43 -0800

    Revert "update"
    This reverts commit 5da1623b6b5d3cfb676a210364bc6526c528d2e3.

--- static/js/travel_video/filter.js
+++
@@ -1,7 +0,0 @@
-<script>
-document.getElementById('searchInput').addEventListener('input', function() {
-    const searchQuery = this.value.toLowerCase();
-    filterVideos(searchQuery);
-});
-
-    </script>
